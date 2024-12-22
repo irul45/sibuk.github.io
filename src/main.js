fd0dccaa9dde172dbd9849f5d205e2de9b1eddb4
irul45 <hudairul314@gmail.com>
2024-12-22 23:56:54 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+let warna = ['purple','yellow'];
+function klik1(){
+    document.body.style.background = warna[1]
+
+}
+function klik2(){
+    document.body.style.background = warna[0]
+    
+}
+
+// let nama ="ahmad khoirul huda"
+// alert("ahmad khoirul huda")
